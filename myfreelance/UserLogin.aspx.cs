2e60efe9d2827939bf03f99daeeb9e0f34a47ae0
Soviet dev <73458675+daboi09@users.noreply.github.com>
2023-02-16 21:40:24 +0800

    Update UserLogin.aspx.cs

--- myfreelance/UserLogin.aspx.cs
+++ myfreelance/UserLogin.aspx.cs
@@ -28,14 +28,11 @@ namespace myfreelance
                     newpass = acct.CheckPassword(loginID);
                     if (newpass != null)
                     {
-                        System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
-                        System.Text.Decoder utf8Decode = encoder.GetDecoder();
-                        byte[] todecode_byte = Convert.FromBase64String(newpass);
-                        int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
-                        char[] decoded_char = new char[charCount];
-                        utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
-                        string result = new String(decoded_char);
-                        if (result == nowpass)
+                        string enpass = user_password_text.Text;
+                        byte[] encData_byte = new byte[enpass.Length];
+                        encData_byte = System.Text.Encoding.UTF8.GetBytes(enpass);
+                        string encodedData = Convert.ToBase64String(encData_byte);
+                        if (encodedData == newpass)
                             {
                                 checkotp = acct.CheckOTP(loginID);
                                 if (checkotp == "Yes")
